c68c73594c3d8de93e4e10b64a4eda20eea15dad
Francesco E. Vallone <francesco.edo.vallone@gmail.com>
2025-04-09 18:04:46 +0200

    add library message

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Welcome to TranscripTools!",
+    "Documentation: https://francesco-e-vallone.github.io/TranscripTools/"
+  )
+}
